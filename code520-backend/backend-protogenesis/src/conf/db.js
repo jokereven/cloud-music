fafaf5c923f0717a3da55692659a37242252d671
jokereven125 <zhou125disorder@163.com>
2021-07-14 22:49:52 +0800

    2021/7/14 add the redis

--- code520-backend/backend-protogenesis/src/conf/db.js
+++ code520-backend/backend-protogenesis/src/conf/db.js
@@ -3,6 +3,7 @@ const env = process.env.NODE_ENV;
 
 // 配置
 let MYSQL_CONF;
+let REDIS_CONF;
 
 if (env === 'dev') {
 	// mysql
@@ -12,6 +13,12 @@ if (env === 'dev') {
 		password: 'root',
 		port: '3306',
 		database: 'code520',
+	};
+
+	//redis
+	REDIS_CONF = {
+		port: 6379,
+		host: '127.0.0.1',
 	};
 }
 
@@ -24,8 +31,15 @@ if (env === 'production') {
 		port: '3306',
 		database: 'code520',
 	};
+
+	//redis
+	REDIS_CONF = {
+		port: 6379,
+		host: '127.0.0.1',
+	};
 }
 
 module.exports = {
 	MYSQL_CONF,
+	REDIS_CONF,
 };
