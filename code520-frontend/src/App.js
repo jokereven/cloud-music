0ad5461b71b0a2f0f2de2db1cd64b4336967996c
jokereven125 <zhou125disorder@163.com>
2021-07-14 21:26:31 +0800

    Revert "code520"
    This reverts commit 95a6250dd62006ca58eee98732af01c76cad3e35.

--- code520-frontend/src/App.js
+++ code520-frontend/src/App.js
@@ -1,7 +1,21 @@
+import axios from 'axios';
 import React, { Component, Fragment } from 'react';
 class App extends Component {
 	render() {
 		return <Fragment></Fragment>;
+	}
+	componentDidMount() {
+		axios
+			.get('http://localhost:3000/api/blog/list')
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.error(error);
+			})
+			.finally(() => {
+				console.log('ok');
+			});
 	}
 }
 
