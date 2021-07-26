3049e68f8d53da5d58851e93d1c712b99178f798
jokereven125 <zhou125disorder@163.com>
2021-07-26 20:37:39 +0800

    2021/7/26 20:37

--- music.code520.com.cn/src/App.js
+++ music.code520.com.cn/src/App.js
@@ -1,9 +1,5 @@
 import React, { Fragment, PureComponent } from 'react';
 /* 全局样式 */
-import { BrowserRouter, Route } from 'react-router-dom';
-import Footer from './common/footer/index';
-/* 组件 */
-import Header from './common/header/index';
 import { GlobalStyle } from './style';
 
 class App extends PureComponent {
@@ -12,10 +8,6 @@ class App extends PureComponent {
 			<Fragment>
 				{/* 全局样式 */}
 				<GlobalStyle />
-				<BrowserRouter>
-					<Route path='/' component={Header}></Route>
-					<Route path='/' component={Footer}></Route>
-				</BrowserRouter>
 			</Fragment>
 		);
 	}
--- music.code520.com.cn/src/content/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment, PureComponent } from 'react';
-
-class Content extends PureComponent {
-	render() {
-		return <Fragment></Fragment>;
-	}
-}
-
-export default Content;
