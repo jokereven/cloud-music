069e02c92b91363e502025814a6959f6e9dab083
jokereven125 <zhou125disorder@163.com>
2021-07-26 22:25:50 +0800

    2021/7/26 22:25

--- music.code520.com.cn/src/content/main/index.js
+++ music.code520.com.cn/src/content/main/index.js
@@ -1,8 +1,25 @@
 import React, { Fragment, PureComponent } from 'react';
+import {
+	MainFindWapper,
+	MainFindWapperNav,
+	MainFindWapperNavList,
+} from './style';
 
 class Main extends PureComponent {
 	render() {
-		return <Fragment></Fragment>;
+		return (
+			<Fragment>
+				<MainFindWapper>
+					<MainFindWapperNav>
+						<MainFindWapperNavList>推荐</MainFindWapperNavList>
+						<MainFindWapperNavList>歌单</MainFindWapperNavList>
+						<MainFindWapperNavList>排行榜</MainFindWapperNavList>
+						<MainFindWapperNavList>歌手</MainFindWapperNavList>
+						<MainFindWapperNavList>最新音乐</MainFindWapperNavList>
+					</MainFindWapperNav>
+				</MainFindWapper>
+			</Fragment>
+		);
 	}
 }
 
